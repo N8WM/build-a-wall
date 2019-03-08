46839a96ebc60fab4311cae20edb76ff8797a721
Nathan McCutchen <n8wm@icloud.com>
2019-03-08 11:09:29 -0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,5 @@
-
+function initiate() {
+  var svg = document.getElementById("svg-area");
+  svg.width = document.getElementById("gameplay").offsetWidth;
+  svg.height = document.getElementById("gameplay").offsetHeight;
+}
