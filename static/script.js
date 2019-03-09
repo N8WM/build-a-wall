dd146a351c572997c13f537df12efc2d72d31a7d
Nathan McCutchen <n8wm@icloud.com>
2019-03-08 22:06:22 -0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,13 +1,11 @@
-var gameplaySize = 0;
+var gameplayWidth = 0;
+var gameplayHeight = 0;
 
 function initiate() {
   var gameplay = document.getElementById("gameplay");
   var gameplayContainer = document.getElementById("gameplay-container");
-  if (gameplayContainer.offsetWidth > gameplayContainer.offsetHeight) {
-    gameplaySize = gameplayContainer.offsetHeight * 0.9;
-  } else {
-    gameplaySize = gameplayContainer.offsetWidth * 0.9;
-  }
+  gameplayWidth = gameplayContainer.offsetWidth * 0.9;
+  gameplayHeight = gameplayWidth * 0.5625;
   gameplay.style.width = gameplaySize + "px";
   gameplay.style.height = gameplaySize + "px";
 }
