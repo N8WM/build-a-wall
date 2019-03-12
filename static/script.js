253345c4263e88ef39d789f2fcfdf11d1e7f03eb
Nathan McCutchen <n8wm@icloud.com>
2019-03-12 12:54:14 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,4 +1,8 @@
 var viewport = (9.0/16.0);
+var transVPX = 144.0;
+var transVPY = 256.0;
+
+// viewport translated: (144, 256) radius
 
 var gameplayWidth = 0;
 var gameplayHeight = 0;
@@ -15,4 +19,24 @@ function initiate() {
   }
   gameplay.style.width = gameplayWidth + "px";
   gameplay.style.height = gameplayHeight + "px";
+}
+
+function run() {
+  window.requestAnimationFrame(run);
+}
+
+function getRealX(x) {
+  return ((x + transVPX) / (2.0 * transVPX)) * gameplayWidth;
+}
+
+function getRealY(y) {
+  return gameplayHeight - ((y + transVPY) / (2.0 * transVPY)) * gameplayHeight;
+}
+
+function getEasyX(x) {
+  return ((x / gameplayWidth) * (2.0 * transVPX)) - transVPX;
+}
+
+function getEasyY(y) {
+  return (((gameplayHeight - y) / gameplayHeight) * (2.0 * transVPY)) - transVPY;
 }
