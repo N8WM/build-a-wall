31c74b744587cb58ff36fc9ccd22581422dbde14
Nathan McCutchen <n8wm@icloud.com>
2019-03-14 12:35:25 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -21,9 +21,9 @@ function initiate() {
   gameplay.style.width = gameplayWidth + "px";
   gameplay.style.height = gameplayHeight + "px";
   svg.innerHTML = "";
-  for (var i = 0; i < 16; i++) {
+  for (var i = 0; i < 17; i++) {
     for (var j = 0; j < 9; j++) {
-      svg.innerHTML += "<image xlink:href='https://github.com/N8WM/build-a-wall/blob/master/brick.png?raw=true' width='" + getRealX(16) + "' x='" + getRealX(i*16) + "' y='" + getRealY(j*16) + "'/>";
+      svg.innerHTML += "<image xlink:href='https://github.com/N8WM/build-a-wall/blob/master/brick.png?raw=true' width='" + getRealX(16) + "' x='" + getRealX(i*16 - ((j % 2) * 8)) + "' y='" + getRealY(j*16) + "'/>";
     }
   }
 }
