e9b769cbd24876d65069ba8feaa753c96a6adcd4
Nathan McCutchen <n8wm@icloud.com>
2019-04-02 17:57:56 -1000

    Testing

--- static/script.js
+++ static/script.js
@@ -67,6 +67,7 @@ function run() {
   }
   manager.on('dir:up', function (evt, data) {
     up = true;
+    console.log("up");
   });
   var ctx = document.getElementById("canvs").getContext("2d");
   ctx.clearRect(0, 0, gameplayWidth, gameplayHeight);
