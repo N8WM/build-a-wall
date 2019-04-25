c8bcb0a227d14382c24abe40fa46b1b4ff0278f2
Nathan McCutchen <n8wm@icloud.com>
2019-04-25 08:28:46 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -157,7 +157,7 @@ function join() {
   } else if (joinInvalid) {
     end();
   } else {
-    tempRun();   // to be changed to "run();" in the end
+    run();   // to be changed to "run();" in the end
   }
 }
 
