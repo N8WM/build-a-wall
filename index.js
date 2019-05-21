39c9f35bf7d2dbce5fdd37433aa6745f41364bcd
Nathan McCutchen <n8wm@icloud.com>
2019-05-21 12:48:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -123,9 +123,9 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket) {
   /**
-  socket.emit('request', ####); // emit an event to the socket
-  io.emit('broadcast', ####); // emit an event to all connected sockets
-  socket.on('reply', function(){ #### }); // listen to the event
+  socket.emit('request', ###); // emit an event to the socket
+  io.emit('broadcast', ###); // emit an event to all connected sockets
+  socket.on('reply', function(){ ### }); // listen to the event
   **/
   socket.on('join', function() {
     var vrooms = rooms.getVacantRooms();
